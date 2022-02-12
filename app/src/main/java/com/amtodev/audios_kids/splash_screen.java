4ad0b54c79a94bf18e8c2d8e5e1aa6e990f1d22c
manasesortez <manasesortez1234@hotmail.com>
2022-02-12 10:22:27 -0600

    audios kids

---
+++ app/src/main/java/com/amtodev/audios_kids/splash_screen.java
@@ -0,0 +1,15 @@
+package com.amtodev.audios_kids;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class splash_screen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
